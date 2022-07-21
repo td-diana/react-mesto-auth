9e8e31cb8e7556e884f17029f5ceac920bc1c887
Evgeny Temirkaev <td-diana.ru@yandex.ru>
2022-07-21 09:51:34 +0300

    модуль API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,26 @@
+import React from 'react'
+import { api } from '../utils/Api'
+
 function Main({ onEditAvatar, onAddPlace, onEditProfile }) {
+  const [userName, setUserName] = React.useState('sadf')
+  const [userDescription, setUserDescription] = React.useState()
+  const [userAvatar, setUserAvatar] = React.useState()
+
+  React.useEffect(() => {
+    api.getUserInfo().then((data) => {      
+      setUserName(data.name)
+      setUserDescription(data.about)
+      setUserAvatar(data.avatar)
+    })
+}, [])
+
   return (
     <main>
       <section className="profile">
         <div className="profile__container">
           <div className="profile__avatar-container">
-            <img className="profile__avatar" alt="" />
+            <img className="profile__avatar" alt="" style={{ backgroundImage: `url(${userAvatar})` }}  />
+            {/* src={userAvatar} */}
             <button
               type="button"
               className="profile__button-avatar-edit"
@@ -14,14 +30,14 @@ function Main({ onEditAvatar, onAddPlace, onEditProfile }) {
         </div>
         <div className="profile__info">
           <div className="profile__container">
-            <h1 className="profile__name"></h1>
+            <h1 className="profile__name">{userName}</h1>
             <button
               type="button"
               className="profile__button-edit"
               onClick={onEditProfile}
             ></button>
           </div>
-          <h2 className="profile__about-name"></h2>
+          <h2 className="profile__about-name">{userDescription}</h2>
         </div>
         <button
           type="button"
---
+++ src/utils/Api.js
@@ -0,0 +1,102 @@
+class Api {
+    constructor(options) {
+      this._url = options.url
+      this._headers = options.headers
+    }
+  
+    _checkResponse(res) {
+      if (res.ok) {
+        return res.json()
+      }
+      return Promise.reject(`Ошибка: ${res.status}`)
+    }
+  
+    getUserInfo() {
+      return fetch(this._url + '/users/me', {
+        method: 'GET',
+        headers: this._headers
+      })
+        .then(this._checkResponse)
+    }
+  
+    getInitialCards() {
+      return fetch(this._url + '/cards', {
+        method: 'GET',
+        headers: this._headers
+      })
+      .then(this._checkResponse)
+    }
+  
+    setUserInfoApi(userData) {
+      return fetch(this._url + '/users/me', {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({
+          name: userData.userName,
+          about: userData.userAbout
+        })
+      })
+      .then(this._checkResponse)
+    }
+  
+    addUserCard(data) {
+      return fetch(this._url + '/cards', {
+        method: 'POST',
+        headers: this._headers,
+        body: JSON.stringify({
+          name: data.name,
+          link: data.link
+        })
+      })
+      .then(this._checkResponse)
+    }
+  
+    like(id) {
+      return fetch(this._url + `/cards/likes/${id}`, {
+        method: 'PUT',
+        headers: this._headers
+      })
+      .then(this._checkResponse)
+    }
+  
+    dislike(id) {
+      return fetch(this._url + `/cards/likes/${id}`, {
+        method: 'DELETE',
+        headers: this._headers
+      })
+      .then(this._checkResponse)
+    }
+  
+    delete(id) {
+      return fetch(this._url + `/cards/${id}`, {
+        method: 'DELETE',
+        headers: this._headers
+      })
+      .then(this._checkResponse)
+    }
+  
+    handleUserAvatar(data) {
+      return fetch(this._url + `/users/me/avatar`, {
+        method: 'PATCH',
+        headers: this._headers,
+        body: JSON.stringify({
+          avatar: data.userAvatar,
+        })
+      })
+      .then(this._checkResponse)
+    }
+  
+    getAllNeededData() {
+      return Promise.all([this.getInitialCards(), this.getUserInfo()])
+    }
+  }
+
+  const api = new Api({
+    url: "https://nomoreparties.co/v1/cohort-44/",
+    headers: {
+      authorization: "cd3dfc8b-e143-41af-a84b-cea0d68401cf",
+      "Content-Type": "application/json",
+    },
+  });
+
+  export { api }
