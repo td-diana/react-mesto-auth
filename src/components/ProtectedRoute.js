5df17e0dd0b32385657a2aaef86f33db6d3ff281
td-diana <99271477+td-diana@users.noreply.github.com>
2022-10-05 11:36:29 +0300

    Updates

--- src/components/ProtectedRoute.js
+++
@@ -1,7 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-const ProtectedRoute = ({ children, loggedIn }) => {
-  return loggedIn ? children : <Navigate to="/sign-in" />;
-};
-
-export default ProtectedRoute;
